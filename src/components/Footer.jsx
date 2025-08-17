f331502a6647ff6077a4f67de472d77e82b4fe71
mhdansif <mohamedansif2003@gmail.com>
2025-08-17 21:34:15 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100 text-center py-4 mt-8 border-t border-gray-300">
+      <p className="text-gray-600 text-sm">
+        MOHAMED ANSIF &copy; 2025. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer; 
+
